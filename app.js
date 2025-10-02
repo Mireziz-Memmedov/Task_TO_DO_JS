5ec748771b4d46b69b7aa93fc9e9801d4f992124
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-10-02 12:11:42 +0000

    todo

--- app.js
+++ app.js
@@ -2,25 +2,19 @@
 $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
-
         if (!$.trim($('input').val()) == '') {
-
-            let n = (m) => {
-                for (let i = 1; i <= m; i++) {
-                    $('.col-md-12').append(`<div class="minibox">${m}</div>`);
+            let a = b => {
+                for (let i = 1; i <= b; i++) {
+                    $('.minibox').append(`<h2>${b}</h2>`);
                 }
             }
+            let v = f => Number(f)
+                ? a(f)
+                : $('.minibox').append(`<h2>${f}</h2>`);
 
-            let s = (h) => isNaN(h)
-                ? $('.col-md-12').append(`<div class="minibox">${h}</div>`)
-                : '';
-
-            n($('input').val());
-            s($('input').val());
+            v($('input').val());
         }
-
         $('input').val('');
-
     });
 });
 
